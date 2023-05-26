1e448ecddf3010cb1ef831449b2a5ae5d8b397c8
rafaelafms <61976923+rafaelafms@users.noreply.github.com>
2023-05-26 17:57:07 -0300

    clienteRPC.py

--- clienteRPC.py
+++ clienteRPC.py
@@ -1,18 +1,34 @@
-# Cliente de calculadora usando RPyC
+# Cliente dicionario usando RPyC
 import rpyc
+import json
 
-dic = rpyc.connect('localhost', 10000)
+def lerDicionario():
+	with open("entrada.json","r") as openfile:
+		atual = json.load(openfile)
+	return atual
+
+def escreverDicionario(dicionario):
+	with open("entrada.json", "w") as outfile:
+		json.dump(dicionario, outfile)
+	return 
+
+dic = rpyc.connect('localhost', 10000, config={'allow_public_attrs': True, 'sync_request_timeout': 10})
 while True:
-	acao = input("Digite uma acao('ler', 'escrever', 'remover' ou 'fim' para terminar): ")
+	acao = input("Digite uma acao('ler', 'escrever', 'remover', 'salvar' ou 'fim' para terminar): ")
 	if acao == 'fim':
 		dic.close()
 		break
 	chave = input("Digite a chave: ")
+	dicionario = lerDicionario()
 	if acao == 'ler':
-		resultado = dic.root.ler(chave)
+		resultado = dic.root.ler(chave, dicionario)
 	elif acao == 'escrever':
 		valor = input("Digite o valor: ")
-		resultado = dic.root.escrever(chave, valor)
+		resultado = dic.root.escrever(chave, valor, dicionario)
+		escreverDicionario(dicionario)
 	elif acao == 'remover':
-		resultado = dic.root.remover(chave)
+		resultado = dic.root.remover(chave, dicionario)
+		escreverDicionario(dicionario)
+	elif acao == 'salvar':
+		escreverDicionario(dicionario)
 	print(resultado)
